0a5797787836b8b537a4dd38e950675fc75f31a2
muazamhussain7 <muazam.hussain@venturedive.com>
2021-04-01 21:06:38 +0500

    assets path added into pubspec.yaml file

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+  List _chats = [
+    {"profile": "", "name": "", "message": "", "time": ""},
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("WhatsAppClone"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () => null,
+          ),
+        ],
+      ),
+      body: Text("This is body"),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => null,
+        child: Icon(Icons.message),
+      ),
     );
   }
 }

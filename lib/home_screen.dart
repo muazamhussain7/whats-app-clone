f0c1f2da22b6b635e0972fc2a6b8e22e7ce5e810
muazamhussain <muazamhussain7@gmail.com>
2021-04-02 10:21:33 +0500

    SafeArea property added into home_screen file.

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -9,42 +9,44 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-            title: Text("WhatsAppClone"),
-            actions: [
-              IconButton(
-                icon: Icon(Icons.search),
-                onPressed: () => null,
-              ),
-              IconButton(
-                icon: Icon(Icons.more_vert),
-                onPressed: () => null,
-              ),
-            ],
-            bottom: TabBar(
-              tabs: [
-                Tab(text: "CHATS"),
-                Tab(text: "STATUS"),
-                Tab(text: "CALLS"),
-              ],
-              indicator: UnderlineTabIndicator(
-                borderSide: BorderSide(
-                  width: 3,
-                  color: Colors.white,
+      child: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+              title: Text("WhatsAppClone"),
+              actions: [
+                IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () => null,
                 ),
-              ),
-            )),
-        body: TabBarView(
-          children: [
-            ChatsScreen(),
-            StatusScreen(),
-            CallsScreen(),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => null,
-          child: Icon(Icons.message),
+                IconButton(
+                  icon: Icon(Icons.more_vert),
+                  onPressed: () => null,
+                ),
+              ],
+              bottom: TabBar(
+                tabs: [
+                  Tab(text: "CHATS"),
+                  Tab(text: "STATUS"),
+                  Tab(text: "CALLS"),
+                ],
+                indicator: UnderlineTabIndicator(
+                  borderSide: BorderSide(
+                    width: 3,
+                    color: Colors.white,
+                  ),
+                ),
+              )),
+          body: TabBarView(
+            children: [
+              ChatsScreen(),
+              StatusScreen(),
+              CallsScreen(),
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () => null,
+            child: Icon(Icons.message),
+          ),
         ),
       ),
     );
